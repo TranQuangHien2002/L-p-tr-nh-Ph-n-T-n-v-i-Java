1042adbca0513d6e2e3f298daea73e315582bc05
TranQuangHien <tqhien27102002@gmail.com>
2023-12-27 22:02:25 +0700

    test

---
+++ BaiText/src/a.java
@@ -0,0 +1,14 @@
+/*
+ * @ (#) a.java   1.0     12/27/2023
+ *
+ * Copyright (c) 2023 IUH. All rights reserved.
+ */
+
+/*
+ * @description:
+ * @author: Tran Quang Hien
+ * @date:   12/27/2023
+ * @version:    1.0
+ */
+public class a {
+}
